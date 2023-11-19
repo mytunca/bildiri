99a697fdc62f36d818dd12564b2a8a880d359a2f
Muhammet Yunus TUNCA <muhammedytunca@gmail.com>
2023-11-19 04:11:36 +0300

    Test

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Varsayılan dil: Türkçe
-  let currentLanguage = 'tr';
-
   // Dil seçimini kontrol et
   const languageSelectors = document.querySelectorAll('.flag');
   languageSelectors.forEach(flag => {
     flag.addEventListener('click', e => {
-      if (currentLanguage == flag.dataset.lang) return;
+      if (i18n.currentLanguage == flag.dataset.lang) return;
 
-      currentLanguage = flag.dataset.lang;
-      updateText();
+      i18n.currentLanguage = flag.dataset.lang;
     });
   });
 
@@ -17,72 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
     SignatureTable.filter = e.target.value;
   })
 
-  // Metin ve tabloyu güncelle
-  function updateText() {
-    const declarationText = document.getElementById('declaration-text');
+  //i18n.currentLanguage = "tr";
 
-    // Önceki içerikleri temizle
-    declarationText.innerHTML = '';
-
-    // Metin dosyasını oku ve içeriği ekle
-    fetch(`assets/content/${currentLanguage}.md`)
-      .then(response => response.text())
-      .then(data => {
-        // Showdown.js'yi kullanarak Markdown içeriğini HTML'ye dönüştür
-        const converter = new showdown.Converter();
-        const htmlContent = converter.makeHtml(data);
-        declarationText.innerHTML = htmlContent;
-      })
-      .catch(error => {
-        console.error('Hata:', error);
-      });
-    
-    //Sayfadaki diğer metin içeriklerini güncelle
-    fetch(`assets/i18n/${currentLanguage}.json`)
-      .then(response => response.json())
-      .then(languageData => {
-        document.lang = currentLanguage;
-        if (currentLanguage == "ar") document.dir = "rtl";
-        else document.dir = "";
-
-        const elementsToUpdate = [
-          {
-            id: "navbar-title",
-            toUpdate: "innerHTML"
-          },
-          {
-            id: "th-name",
-            toUpdate: "innerHTML"
-          },
-          {
-            id: "th-signature-date",
-            toUpdate: "innerHTML"
-          },
-          {
-            id: "signature-list-caption",
-            toUpdate: "innerHTML"
-          },
-          {
-            id: "filter",
-            toUpdate: "placeholder"
-          },
-        ];
-
-        console.log(elementsToUpdate);
-
-        elementsToUpdate.forEach(el => {
-          console.log(el);
-          document.getElementById(el.id)[el.toUpdate] = languageData[el.id];
-        })
-
-      })
-      .catch(error => {
-        console.error('Hata:', error);
-      });
-  }
-
-  // Sayfa yüklendiğinde metni ve tabloyu güncelle
-  updateText();
   SignatureTable.getData();
 });
 
@@ -90,8 +22,11 @@ class SignatureTable {
   static el = document.querySelector('#signature-list');
   static data = [];
 
-  static getData() {
-    this.el.innerHTML = '<tr><td></td><td colspan="2">İsimler yükleniyor...</td></tr>';
+  static async getData() {
+    const langData = await i18n.getData();
+    const loading = langData["loading"];
+
+    this.el.innerHTML = `<tr><td></td><td colspan="2" id="loading">${loading}</td></tr>`;
     const url = "https://script.google.com/macros/s/AKfycby8oix6aoonJOVvJNCqtARCzoTFltj6fhvGEHB361tqJEY5WWWqo-PsEXY3Lv8Rlx4E/exec";
     fetch(url)
       .then(x => x.json())
@@ -131,6 +66,95 @@ class SignatureTable {
   static set filter(text) {
     this._filter = text.replaceAll(" ", "");
     this.render();
+  }
+
+}
+
+class Content {
+  static declarationText = document.getElementById('declaration-text');
+
+  static update() {
+    // Önceki içerikleri temizle
+    this.declarationText.innerHTML = '';
+
+    // Metin dosyasını oku ve içeriği ekle
+    fetch(`assets/content/${i18n.currentLanguage}.md`)
+      .then(response => response.text())
+      .then(data => {
+        // Showdown.js'yi kullanarak Markdown içeriğini HTML'ye dönüştür
+        const converter = new showdown.Converter();
+        const htmlContent = converter.makeHtml(data);
+        this.declarationText.innerHTML = htmlContent;
+      })
+      .catch(error => {
+        console.error('Hata:', error);
+      });
+  }
+}
+
+class i18n {
+  static _data = {};
+  static _currentLanguage = 'tr';
+  static elementsToUpdate = [
+    {
+      id: "navbar-title",
+      toUpdate: "innerHTML"
+    },
+    {
+      id: "th-name",
+      toUpdate: "innerHTML"
+    },
+    {
+      id: "th-signature-date",
+      toUpdate: "innerHTML"
+    },
+    {
+      id: "signature-list-caption",
+      toUpdate: "innerHTML"
+    },
+    {
+      id: "filter",
+      toUpdate: "placeholder"
+    },
+    {
+      id: "loading",
+      toUpdate: "innerHTML"
+    },
+  ];
+
+  /**
+   * @param {String} lang
+   */
+  static set currentLanguage(lang) {
+    this._currentLanguage = lang;
+    document.documentElement.lang = lang;
+    if (lang == "ar") document.dir = "rtl";
+    else document.dir = "";
+    this.applyLanguage();
+
+  }
+
+  static get currentLanguage() { return this._currentLanguage }
+
+  static async getData() {
+    if (!this._data[this.currentLanguage]) {
+      const response = await fetch(`assets/i18n/${this.currentLanguage}.json`);
+      const languageData = await response.json();
+
+      this._data[this.currentLanguage] = languageData;
+    }
+
+    return this._data[this.currentLanguage];
+  }
+
+  static async applyLanguage() {
+    Content.update();
+
+    const languageData = await this.getData();
+    this.elementsToUpdate.forEach(el => {
+      const elToChange = document.getElementById(el.id);
+      elToChange && (elToChange[el.toUpdate] = languageData[el.id])
+    })
   }
 
 }
